a22fab6e5a20e878352e750d97f7ae3f6194b922
Dom.Hofer <dmand6666@gmail.com>
2020-11-17 12:55:23 +0100

    added a User Object

--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.mobileappws.ui.model.response.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,14 +9,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping()
-    public String getUser(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit", defaultValue="50") int limit) {
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "50") int limit, @RequestParam(value = "sort", required = false, defaultValue = "desc") String sort) {
 
-        return "get user was called with page = " + page + " and limit = " + limit;
+        return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
     }
 
-    @GetMapping(path="/{userId}")
-    public String getUser(@PathVariable String userId) {
-        return "get user was called with userId = " + userId;
+    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public UserRest getUser(@PathVariable String userId) {
+
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("test@test.com");
+        returnValue.setFirstName("Dominic");
+        returnValue.setLastName("Hofer");
+        returnValue.setUserId(userId);
+        return returnValue;
     }
 
     @PostMapping
